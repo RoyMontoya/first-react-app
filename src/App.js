ab39bfd36084ca285712e361a967958a54a206b5
Roy Montoya <roymontoya89@gmail.com>
2017-11-24 22:40:35 -0600

    added buttons and functionallity

--- src/App.js
+++ src/App.js
@@ -7,25 +7,48 @@ import Image from './components/Image';
 import Input from './components/Input';
 
 class App extends Component {
+  // constructor(){
+  //   super();
+  //
+  //   this.state = { inputType: 'text'}
+  //
+  //   this.onClickChild = this.onClickChild.bind(this)
+  // }
+  //
+  // onClickChild(e){
+  //   alert(0)
+  //   this.setState({inputType: 'number'});
+  // }
   constructor(){
     super();
 
-    this.state = { inputType: 'text'}
+    this.state = {
+      label: "0"
+    };
 
-    this.onClickChild = this.onClickChild.bind(this)
+    this.onClickButton = this.onClickButton.bind(this);
   }
 
-  onClickChild(e){
-    alert(0)
-    this.setState({inputType: 'number'});
+  onClickButton(e){
+    if(e.target.id == "b1"){
+      this.setState({ label: "uno"});
+    }else if(e.target.id == "b2"){
+      this.setState({ label: "dos"});
+    }else{
+      this.setState({ label: "tres"});
+    }
   }
-
 
   render() {
     return (
       <div >
-        <Image id={1} src="" onClickChild = {this.onClickChild}></Image>
-        <Input id={2} type= {this.state.inputType}></Input>
+        {/* <Image id={1} src="" onClickChild = {this.onClickChild}></Image>
+        <Input id={2} type= {this.state.inputType}></Input> */}
+
+        <button id = "b1" onClick = {this.onClickButton}>Click 1</button>
+        <button id = "b2" onClick = {this.onClickButton}>Click 2</button>
+        <button id = "b3" onClick = {this.onClickButton}>Click 3</button>
+        <label className="App-label">{this.state.label}</label>
       </div>
     );
   }
